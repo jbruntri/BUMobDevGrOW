44cc8ebb3448c31e6e69b0f907c2ab6c82f2860e
Justin Bui <justin_bui@baylor.edu>
2017-03-28 10:05:22 -0500

    Creating the base project
    Created some base files to get us started. Rough start to the player and
    mob stats, created some fake upgrades etc.

---
+++ Mage Crawler/mobs.lua
@@ -0,0 +1,18 @@
+-- Soldier stats --
+soldier = {}
+soldier.hp=20;
+soldier.dmg=10;
+soldier.spd=4;
+soldier.armor=0;
+
+shield = {}
+shield.hp=50;
+shield.dmg=30;
+soldier.spd=1;
+shield.armor=5;
+
+boss = {}
+boss.hp=150;
+boss.dmg=20;
+boss.sdmg=50;
+boss.armor=10;
